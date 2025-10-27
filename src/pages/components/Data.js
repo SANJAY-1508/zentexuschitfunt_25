b56e5d2844e6881a66dbfb072939f489afb59806
SANJAY-1508 <psanjaykumar377@gmail.com>
2025-10-27 15:35:15 +0530

    `Removed hardcoded data and imported from components/Data`

---
+++ src/pages/components/Data.js
@@ -0,0 +1,92 @@
+export const fundPlans = [
+  { plan: "₹100 X 45 WEEKS", total: "₹4500 + ₹500", amount: "5000" },
+  { plan: "₹300 X 9 MONTHS", total: "₹2700 + ₹300", amount: "3000" },
+  { plan: "₹500 X 9 MONTHS", total: "₹4500 + ₹500", amount: "5000" },
+  { plan: "₹1000 X 9 MONTHS", total: "₹9000 + ₹1000", amount: "10000" },
+  { plan: "₹2000 X 9 MONTHS", total: "₹18000 + ₹2000", amount: "20000" },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Saved ₹2000 on crackers last year! Easy payments and fast delivery.",
+    name: "Ravi Kumar, Chennai",
+  },
+  {
+    quote: "The bonus months made all the difference. Highly recommend!",
+    name: "Priya S, Coimbatore",
+  },
+  {
+    quote: "Professional service from start to finish. Diwali was explosive!",
+    name: "Arun M, Madurai",
+  },
+];
+
+export const steps = [
+  {
+    stepNumber: 1,
+    heading: "Choose your plan from our plan list",
+    description:
+      "You can choose between plans and you can purchase any number of plans you like. This makes you choose the amount of fund you need for your Diwali plan",
+    imageSrc:
+      "https://asset.iamretailer.com/resto/catalog/view/theme/goodwill/images/plans/analysis.png",
+    imageAlt: "choose",
+    imageClass: "howchoose",
+    position: "right",
+  },
+  {
+    stepNumber: 2,
+    heading: "Submit us the Register form",
+    description:
+      "Submit your information with the registration form presents in this page. Kindly fill the validation communication details to contact you at right time with the information.",
+    imageSrc:
+      "https://asset.iamretailer.com/resto/catalog/view/theme/goodwill/images/plans/record.png",
+    imageAlt: "register",
+    imageClass: "howsubmit",
+    position: "left",
+  },
+  {
+    stepNumber: 3,
+    heading: "Pay with the link/ UPI, Bank transfer or the Account generated",
+    description:
+      "Once the form submitted we will create a separate account for you and you will be received Mail and SMS for your account. you can deposit your every month amount into that. Or you can pay via any of the payment options available in the terms and conditons",
+    imageSrc:
+      "https://asset.iamretailer.com/resto/catalog/view/theme/goodwill/images/plans/card.png",
+    imageAlt: "account",
+    imageClass: "howpay",
+    position: "right",
+  },
+  {
+    stepNumber: 4,
+    heading: "Amount credit to Goodwill Fireworks Crackers wallet",
+    description:
+      "At the end of scheme the amount will be credited into goodwillfireworks.com wallet with the bonus amount and you will be notified via Email and SMS",
+    imageSrc:
+      "https://asset.iamretailer.com/resto/catalog/view/theme/goodwill/images/plans/purse.png",
+    imageAlt: "credit",
+    imageClass: "howamount",
+    position: "left",
+  },
+  {
+    stepNumber: 5,
+    heading: "Login with your mobile number at goodwillfireworks.in",
+    description:
+      "Login with your registered mobile number or email with our site and you can able to view the amount credited into wallet with bonus amount",
+    imageSrc:
+      "https://asset.iamretailer.com/resto/catalog/view/theme/goodwill/images/plans/key.png",
+    imageAlt: "login",
+    imageClass: "howlogin",
+    position: "right",
+  },
+  {
+    stepNumber: 6,
+    heading: "Enjoy the crackers purchase",
+    description:
+      "You can choose your desired products and can purchase with the wallet amount you have in your wallet and Enjoy the celebration!",
+    imageSrc:
+      "https://asset.iamretailer.com/resto/catalog/view/theme/goodwill/images/plans/cart.png",
+    imageAlt: "purchase",
+    imageClass: "howenjoy",
+    position: "left",
+  },
+];
